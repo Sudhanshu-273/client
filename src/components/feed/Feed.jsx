f3133e2d2785c6471966c5979a743788d6c732fa
Sudhanshu <sudhanshuchaubey273@gmail.com>
2022-09-04 11:19:28 +0530

    First Commit

---
+++ src/components/feed/Feed.jsx
@@ -0,0 +1,33 @@
+import Share from "../share/Share";
+import Post from "../post/Post";
+import "./feed.css";
+import {useEffect, useState} from "react";
+import axios from "axios";
+// import {Posts} from "../../dummyData";
+
+export default function Feed({username}) {
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => { 
+      const res = username 
+      ? await axios.get("/posts/profile/" + username) 
+      : await axios.get("posts/timeline/6309ecd03c6cda6b20608965");
+      setPosts(res.data);
+    };
+    fetchPosts();
+  },[username]);
+
+  return (
+    <div className="feed">
+        <div className="feedWrapper">
+          <Share />
+          {posts.map(p => (
+            <Post key={p._id} post={p}/>
+          ))}
+          
+        </div>
+    </div>
+  )
+}
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,37 @@
+import { useRef } from "react";
+import "./login.css";
+
+export default function Login() {
+
+    const email = useRef();
+    const password = useRef();
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        console.log(email);
+    }
+
+    return (
+        <div className="login">
+            <div className="loginWrapper">
+                <div className="loginLeft">
+                    <h3 className="loginLogo">Social Media</h3>
+                    <span className="loginDesc">
+                        Connect with friends around you on Social Media.
+                    </span>
+                </div>
+                <div className="loginRight">
+                    <form className="loginBox" onSubmit={handleClick}>
+                        <input placeholder="Email" type="email" className="loginInput" ref={email} required />
+                        <input placeholder="Password" type="password" className="loginInput" ref={password} required minLength="6" />
+                        <button className="loginButton">Log In</button>
+                        <span className="loginForgot">Forgot Password?</span>
+                        <button className="loginRegisterButton">
+                            Create a New Account
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+}
